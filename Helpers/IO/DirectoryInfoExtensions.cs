b84d6f77f3ca13437c9a8d7b914b8d424345ea78
jsmunroe <jsmunroe@gmail.com.com>
2015-09-01 13:36:42 -0500

    Added XmlSet

--- Helpers/IO/DirectoryInfoExtensions.cs
+++ Helpers/IO/DirectoryInfoExtensions.cs
@@ -1,4 +1,5 @@
-﻿using System.IO;
+﻿using System;
+using System.IO;
 
 namespace Helpers.IO
 {
@@ -10,8 +11,20 @@ namespace Helpers.IO
         /// <param name="a_directory">"This" directory.</param>
         /// <param name="a_fileName">File name.</param>
         /// <returns>File info of the child file.</returns>
+        /// <exception cref="NullReferenceException">Thrown if <paramref name="a_directory"/> is null.</exception>
+        /// <exception cref="ArgumentNullException">Thrown if <paramref name="a_fileName"/> is null.</exception>
         public static FileInfo File(this DirectoryInfo a_directory, string a_fileName)
         {
+            #region Argument Validation
+
+            if (a_directory == null)
+                throw new NullReferenceException(nameof(a_directory));
+
+            if (a_fileName == null)
+                throw new ArgumentNullException(nameof(a_fileName));
+
+            #endregion
+
             var filePath = FilePath(a_directory, a_fileName);
 
             return new FileInfo(filePath);
@@ -23,8 +36,20 @@ namespace Helpers.IO
         /// <param name="a_directory">"This" directory.</param>
         /// <param name="a_fileName">File name.</param>
         /// <returns>File path to the child file.</returns>
+        /// <exception cref="NullReferenceException">Thrown if <paramref name="a_directory"/> is null.</exception>
+        /// <exception cref="ArgumentNullException">Thrown if <paramref name="a_fileName"/> is null.</exception>
         public static string FilePath(this DirectoryInfo a_directory, string a_fileName)
         {
+            #region Argument Validation
+
+            if (a_directory == null)
+                throw new NullReferenceException(nameof(a_directory));
+
+            if (a_fileName == null)
+                throw new ArgumentNullException(nameof(a_fileName));
+
+            #endregion
+
             return Path.Combine(a_directory.FullName, a_fileName);
         }
 
@@ -34,8 +59,20 @@ namespace Helpers.IO
         /// <param name="a_directory">"This" directory.</param>
         /// <param name="a_directoryName">Directory name.</param>
         /// <returns>Directory info of the child directory.</returns>
+        /// <exception cref="NullReferenceException">Thrown if <paramref name="a_directory"/> is null.</exception>
+        /// <exception cref="ArgumentNullException">Thrown if <paramref name="a_directoryName"/> is null.</exception>
         public static DirectoryInfo Directory(this DirectoryInfo a_directory, string a_directoryName)
         {
+            #region Argument Validation
+
+            if (a_directory == null)
+                throw new NullReferenceException(nameof(a_directory));
+
+            if (a_directoryName == null)
+                throw new ArgumentNullException(nameof(a_directoryName));
+
+            #endregion
+
             var directoryPath = DirectoryPath(a_directory, a_directoryName);
 
             return new DirectoryInfo(directoryPath);
@@ -47,8 +84,20 @@ namespace Helpers.IO
         /// <param name="a_directory">"This" directory.</param>
         /// <param name="a_directoryName">Directory name.</param>
         /// <returns>Directory path to the child directory.</returns>
+        /// <exception cref="NullReferenceException">Thrown if <paramref name="a_directory"/> is null.</exception>
+        /// <exception cref="ArgumentNullException">Thrown if <paramref name="a_directoryName"/> is null.</exception>
         public static string DirectoryPath(this DirectoryInfo a_directory, string a_directoryName)
         {
+            #region Argument Validation
+
+            if (a_directory == null)
+                throw new NullReferenceException(nameof(a_directory));
+
+            if (a_directoryName == null)
+                throw new ArgumentNullException(nameof(a_directoryName));
+
+            #endregion
+
             return Path.Combine(a_directory.FullName, a_directoryName);
         }
     }
---
+++ Helpers/Xml/IFileSet.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+
+namespace Helpers.Xml
+{
+    public interface IFileSet<TEntity> : ICollection<TEntity>
+    {
+        /// <summary>
+        /// Save the set to its file.
+        /// </summary>
+        void Save();
+
+        /// <summary>
+        /// Revert the set back to its file.
+        /// </summary>
+        void Revert();
+    }
+}
