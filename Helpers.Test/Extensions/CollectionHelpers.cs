623f6eae2d380e0b7b5de2d389ef690ceb594551
jsmunroe <jsmunroe@gmail.com.com>
2015-09-10 13:55:17 -0500

    Added CollectionHelpers class with extension methods for ICollection(T)

---
+++ Helpers.Test/Extensions/CollectionHelpers.cs
@@ -0,0 +1,137 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Helpers.Extensions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Helpers.Test.Extensions
+{
+    [TestClass]
+    public class CollectionHelpersTest
+    {
+
+        [TestMethod]
+        public void AddRange()
+        {
+            // Setup
+            var collection = new Collection<string>();
+            
+            // Execute
+            collection.AddRange("One", "Two", "Three", "Four");
+
+            // Assert
+            CollectionAssert.AreEquivalent(new[] {"One", "Two", "Three", "Four"}, collection);
+
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(NullReferenceException))]
+        public void AddRangeOnNull()
+        {
+            // Setup
+            ICollection<int> collection = null;
+
+            // Execute
+            collection.AddRange(1, 2, 3, 4);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentNullException))]
+        public void AddRangeWithNull()
+        {
+            // Setup
+            var collection = new Collection<string>();
+
+            // Execute
+            collection.AddRange(a_items: null);
+        }
+
+
+        [TestMethod]
+        public void RemoveRange()
+        {
+            // Setup
+            var collection = new Collection<string>();
+            collection.AddRange("One", "Two", "Three", "Four");
+
+            // Execute
+            collection.RemoveRange("Two", "Three");
+
+            // Assert
+            CollectionAssert.AreEquivalent(new[] { "One", "Four" }, collection);
+        }
+
+
+        [TestMethod]
+        [ExpectedException(typeof(NullReferenceException))]
+        public void RemoveRangeOnNull()
+        {
+            // Setup
+            ICollection<int> collection = null;
+
+            // Execute
+            collection.RemoveRange(1, 2, 3, 4);
+        }
+
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentNullException))]
+        public void RemoveRangeWithNull()
+        {
+            // Setup
+            var collection = new Collection<string>();
+
+            // Execute
+            collection.RemoveRange(a_items: null);
+        }
+
+        [TestMethod]
+        public void RemoveWhere()
+        {
+            // Setup
+            var collection = new Collection<string>();
+            collection.AddRange("One", "Two", "Three", "Four");
+
+            // Execute
+            collection.RemoveWhere(i => i.Length > 3);
+
+            // Assert
+            CollectionAssert.AreEquivalent(new[] { "One", "Two" }, collection);
+        }
+
+
+        [TestMethod]
+        [ExpectedException(typeof(NullReferenceException))]
+        public void RemoveWhereOnNull()
+        {
+            // Setup
+            ICollection<int> collection = null;
+
+            // Execute
+            collection.RemoveWhere(i => i > 5);
+        }
+
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentNullException))]
+        public void RemoveWhereWithNull()
+        {
+            // Setup
+            var collection = new Collection<string>();
+
+            // Execute
+            collection.RemoveWhere(a_predicate: null);
+        }
+
+
+
+
+
+
+
+
+    }
+}
---
+++ Helpers/Extensions/CollectionHelpers.cs
@@ -0,0 +1,134 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Helpers.Extensions
+{
+    public static class CollectionHelpers 
+    {
+        /// <summary>
+        /// Add the given range of items (<paramref name="a_items"/>) to "this" collection (<paramref name="a_this"/>).
+        /// </summary>
+        /// <typeparam name="TItem">Type of item.</typeparam>
+        /// <param name="a_this">"This" collection.</param>
+        /// <param name="a_items">Sequence of items to add.</param>
+        /// <exception cref="NullReferenceException">Thrown if <paramref name="a_this"/> is null.</exception>
+        /// <exception cref="ArgumentNullException">Thrown if <paramref name="a_items"/> is null.</exception>
+        public static void AddRange<TItem>(this ICollection<TItem> a_this, IEnumerable<TItem> a_items)
+        {
+            #region Argument Validation
+
+            if (a_this == null)
+                throw new NullReferenceException();
+
+            if (a_items == null)
+                throw new ArgumentNullException(nameof(a_items));
+
+            #endregion
+
+            foreach (var item in a_items)
+                a_this.Add(item);
+        }
+
+        /// <summary>
+        /// Add the given range of items (<paramref name="a_items"/>) to "this" collection (<paramref name="a_this"/>).
+        /// </summary>
+        /// <typeparam name="TItem">Type of item.</typeparam>
+        /// <param name="a_this">"This" collection.</param>
+        /// <param name="a_items">Sequence of items to add.</param>
+        /// <exception cref="NullReferenceException">Thrown if <paramref name="a_this"/> is null.</exception>
+        /// <exception cref="ArgumentNullException">Thrown if <paramref name="a_items"/> is null.</exception>
+        public static void AddRange<TItem>(this ICollection<TItem> a_this, params TItem[] a_items)
+        {
+            #region Argument Validation
+
+            if (a_this == null)
+                throw new NullReferenceException();
+
+            if (a_items == null)
+                throw new ArgumentNullException(nameof(a_items));
+
+            #endregion
+
+            foreach (var item in a_items)
+                a_this.Add(item);
+        }
+
+        /// <summary>
+        /// Remove the given range of items (<paramref name="a_items"/>) to "this" collection (<paramref name="a_this"/>).
+        /// </summary>
+        /// <typeparam name="TItem">Type of item.</typeparam>
+        /// <param name="a_this">"This" collection.</param>
+        /// <param name="a_items">Sequence of items to remove.</param>
+        /// <exception cref="NullReferenceException">Thrown if <paramref name="a_this"/> is null.</exception>
+        /// <exception cref="ArgumentNullException">Thrown if <paramref name="a_items"/> is null.</exception>
+        public static void RemoveRange<TItem>(this ICollection<TItem> a_this, IEnumerable<TItem> a_items)
+        {
+            #region Argument Validation
+
+            if (a_this == null)
+                throw new NullReferenceException();
+
+            if (a_items == null)
+                throw new ArgumentNullException(nameof(a_items));
+
+            #endregion
+
+            foreach (var item in a_items.ToArray())
+                a_this.Remove(item);
+        }
+
+        /// <summary>
+        /// Remove the given range of items (<paramref name="a_items"/>) to "this" collection (<paramref name="a_this"/>).
+        /// </summary>
+        /// <typeparam name="TItem">Type of item.</typeparam>
+        /// <param name="a_this">"This" collection.</param>
+        /// <param name="a_items">Sequence of items to remove.</param>
+        /// <exception cref="NullReferenceException">Thrown if <paramref name="a_this"/> is null.</exception>
+        /// <exception cref="ArgumentNullException">Thrown if <paramref name="a_items"/> is null.</exception>
+        public static void RemoveRange<TItem>(this ICollection<TItem> a_this, params TItem[] a_items)
+        {
+            #region Argument Validation
+
+            if (a_this == null)
+                throw new NullReferenceException();
+
+            if (a_items == null)
+                throw new ArgumentNullException(nameof(a_items));
+
+            #endregion
+
+            foreach (var item in a_items.ToArray())
+                a_this.Remove(item);
+        }
+
+        /// <summary>
+        /// Remove any item in "this" collection (<paramref name="a_this"/>) for which the given predicate (<paramref name="a_predicate"/>) returns true.
+        /// </summary>
+        /// <typeparam name="TItem"></typeparam>
+        /// <param name="a_this">"This" collection.</param>
+        /// <param name="a_predicate">Predicate.</param>
+        /// <exception cref="NullReferenceException">Thrown if <paramref name="a_this"/> is null.</exception>
+        /// <exception cref="ArgumentNullException">Thrown if <paramref name="a_predicate"/> is null.</exception>
+        public static void RemoveWhere<TItem>(this ICollection<TItem> a_this, Predicate<TItem> a_predicate)
+        {
+            #region Argument Validation
+
+            if (a_this == null)
+                throw new NullReferenceException();
+
+            if (a_predicate == null)
+                throw new ArgumentNullException(nameof(a_predicate));
+
+            #endregion
+
+            foreach (var item in a_this.ToArray())
+            {
+                if (a_predicate(item))
+                    a_this.Remove(item);
+            }
+        }
+    }
+}
