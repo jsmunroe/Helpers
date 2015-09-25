ebefb1099fb7f8e4faad8f9315bbcbef53c72155
jsmunroe <jsmunroe@gmail.com.com>
2015-09-25 13:57:52 -0500

    Added `ScopeProvider` to provide action-based support for `using`.

---
+++ Helpers.Test/ScopeProviderTest.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Helpers.Test
+{
+    [TestClass]
+    public class ScopeProviderTest
+    {
+        [TestMethod]
+        public void ConstructScopeProvider()
+        {
+            // Execute
+            new ScopeProvider(ConstructScopeProvider);
+        }
+
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentNullException))]
+        public void ConstructScopeProviderWithNullAction()
+        {
+            // Execute
+            new ScopeProvider(a_disposeAction: null);
+        }
+
+        [TestMethod]
+        public void DisposeScopeProvider()
+        {
+            // Setup
+            var called = false;
+            Action action = () => called = true;
+            var scopeProvider = new ScopeProvider(action);
+
+            // Execute
+            scopeProvider.Dispose();
+
+            // Assert
+            Assert.IsTrue(called);
+        }
+
+
+    }
+}
---
+++ Helpers/ScopeProvider.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace Helpers
+{
+    public class ScopeProvider : IDisposable
+    {
+        private Action _disposeAction;
+
+        /// <summary>
+        /// Constructor.
+        /// </summary>
+        /// <param name="a_disposeAction">Action invoked when this provider is disposed.</param>
+        /// <exception cref="ArgumentNullException">Thrown if <paramref name="a_disposeAction"/> is null.</exception>
+        public ScopeProvider(Action a_disposeAction)
+        {
+            #region Argument Validation
+
+            if (a_disposeAction == null)
+                throw new ArgumentNullException(nameof(a_disposeAction));
+
+            #endregion
+
+            _disposeAction = a_disposeAction;
+        }
+
+        /// <summary>
+        /// Performs application-defined tasks associated with freeing, releasing, or resetting unmanaged resources.
+        /// </summary>
+        public void Dispose()
+        {
+            _disposeAction?.Invoke();
+            _disposeAction = null;
+        }
+    }
+}
