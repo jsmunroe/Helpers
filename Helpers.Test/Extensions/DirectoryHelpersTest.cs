dc0fddbc0464ec90fc1c2be7b0b417f4e73680bd
jsmunroe <jsmunroe@gmail.com.com>
2015-10-28 15:06:41 -0500

    Added `DirectoryHelpers` methods go get descendents of directories.

--- Helpers.Test/Extensions/DirectoryHelpersTest.cs
+++ Helpers.Test/Extensions/DirectoryHelpersTest.cs
@@ -20,14 +20,100 @@ namespace Helpers.Test.Extensions
             _fileSystem.StageDirectory(@"\Root\Directory");
             _fileSystem.StageDirectory(@"\Root\Directory\Sub1");
             _fileSystem.StageDirectory(@"\Root\Directory\Sub1\Sub2");
-            _fileSystem.StageDirectory(@"\Root\Directory\Sub1\Sub2\File1.dat");
-            _fileSystem.StageDirectory(@"\Root\Directory\Sub1\Sub2\File2.dat");
+            _fileSystem.StageFile(@"\Root\Directory\Sub1\Sub2\File1.dat");
+            _fileSystem.StageFile(@"\Root\Directory\Sub1\Sub2\File2.dat");
             _fileSystem.StageDirectory(@"\Root\Directory\Sub1\Sub3");
-            _fileSystem.StageDirectory(@"\Root\Directory\Sub1\Sub3\File1.dat");
-            _fileSystem.StageDirectory(@"\Root\Directory\Sub1\Sub3\File2.dat");
-            _fileSystem.StageDirectory(@"\Root\Directory\Sub1\File1.dat");
-            _fileSystem.StageDirectory(@"\Root\Directory\Sub1\File2.dat");
+            _fileSystem.StageFile(@"\Root\Directory\Sub1\Sub3\File1.dat");
+            _fileSystem.StageFile(@"\Root\Directory\Sub1\Sub3\File2.dat");
+            _fileSystem.StageFile(@"\Root\Directory\Sub1\File1.dat");
+            _fileSystem.StageFile(@"\Root\Directory\Sub1\File2.dat");
         }
+
+
+        [TestMethod]
+        public void GetDescendentDirectories()
+        {
+            // Setup
+            var source = new TestDirectory(_fileSystem, @"\Root\Directory");
+
+            // Execute
+            var result = source.GetDescendentDirectories();
+
+            // Assert
+            Assert.AreEqual(3, result.Length);
+            CollectionAssert.AreEquivalent(new [] {"Sub1", "Sub2", "Sub3"}, result.Select(i => i.Name).ToArray());
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(NullReferenceException))]
+        public void GetDescendentDirectoriesOnNull()
+        {
+            // Setup
+            TestDirectory source = null;
+
+            // Execute
+            var result = source.GetDescendentDirectories();
+        }
+
+
+        [TestMethod]
+        public void GetDescendentDirectoriesWhereNoneExist()
+        {
+            // Setup
+            var source = new TestDirectory(_fileSystem, @"\Root\Directory\sub1\sub3");
+
+            // Execute
+            var result = source.GetDescendentDirectories();
+
+            // Assert
+            Assert.AreEqual(0, result.Length);
+        }
+
+
+        [TestMethod]
+        public void GetDescendentFiles()
+        {
+            // Setup
+            var source = new TestDirectory(_fileSystem, @"\Root\Directory");
+
+            // Execute
+            var result = source.GetDescendentFiles();
+
+            // Assert
+            Assert.AreEqual(6, result.Length);
+            CollectionAssert.AreEquivalent(new[] { "File1.dat", "File1.dat", "File1.dat", "File2.dat", "File2.dat", "File2.dat", }, result.Select(i => i.Name).ToArray());
+        }
+
+
+        [TestMethod]
+        [ExpectedException(typeof(NullReferenceException))]
+        public void GetDescendentFilesOnNull()
+        {
+            // Setup
+            TestDirectory source = null;
+
+            // Execute
+            var result = source.GetDescendentFiles();
+        }
+
+
+        [TestMethod]
+        public void GetDescendentFilesWhereNoneExist()
+        {
+            // Setup
+            _fileSystem.StageDirectory(@"\Root\Directory\Sub1\Sub4");
+            _fileSystem.StageDirectory(@"\Root\Directory\Sub1\Sub4\sub5");
+            _fileSystem.StageDirectory(@"\Root\Directory\Sub1\Sub4\sub6");
+            _fileSystem.StageDirectory(@"\Root\Directory\Sub1\Sub4\sub6\sub7");
+            var source = new TestDirectory(_fileSystem, @"\Root\Directory\sub1\sub4");
+
+            // Execute
+            var result = source.GetDescendentFiles();
+
+            // Assert
+            Assert.AreEqual(0, result.Length);
+        }
+
 
 
         [TestMethod]
@@ -43,13 +129,13 @@ namespace Helpers.Test.Extensions
             // Assert
             Assert.IsTrue(_fileSystem.DirectoryExists(@"\Root\Directory\SubA"));
             Assert.IsTrue(_fileSystem.DirectoryExists(@"\Root\Directory\SubA\Sub2"));
-            Assert.IsTrue(_fileSystem.DirectoryExists(@"\Root\Directory\SubA\Sub2\File1.dat"));
-            Assert.IsTrue(_fileSystem.DirectoryExists(@"\Root\Directory\SubA\Sub2\File2.dat"));
+            Assert.IsTrue(_fileSystem.FileExists(@"\Root\Directory\SubA\Sub2\File1.dat"));
+            Assert.IsTrue(_fileSystem.FileExists(@"\Root\Directory\SubA\Sub2\File2.dat"));
             Assert.IsTrue(_fileSystem.DirectoryExists(@"\Root\Directory\SubA\Sub3"));
-            Assert.IsTrue(_fileSystem.DirectoryExists(@"\Root\Directory\SubA\Sub3\File1.dat"));
-            Assert.IsTrue(_fileSystem.DirectoryExists(@"\Root\Directory\SubA\Sub3\File2.dat"));
-            Assert.IsTrue(_fileSystem.DirectoryExists(@"\Root\Directory\SubA\File1.dat"));
-            Assert.IsTrue(_fileSystem.DirectoryExists(@"\Root\Directory\SubA\File2.dat"));
+            Assert.IsTrue(_fileSystem.FileExists(@"\Root\Directory\SubA\Sub3\File1.dat"));
+            Assert.IsTrue(_fileSystem.FileExists(@"\Root\Directory\SubA\Sub3\File2.dat"));
+            Assert.IsTrue(_fileSystem.FileExists(@"\Root\Directory\SubA\File1.dat"));
+            Assert.IsTrue(_fileSystem.FileExists(@"\Root\Directory\SubA\File2.dat"));
         }
 
 
--- Helpers/Extensions/DirectoryHelpers.cs
+++ Helpers/Extensions/DirectoryHelpers.cs
@@ -1,10 +1,81 @@
 using System;
+using System.Collections.Generic;
+using System.Linq;
 using Helpers.Contracts;
 
 namespace Helpers.Extensions
 {
     public static class DirectoryHelpers
     {
+        /// <summary>
+        /// Enumerate through all descendent directories under "this" directory (<paramref name="a_this"/>).
+        /// </summary>
+        /// <param name="a_this">"This" directory.</param>
+        /// <returns>All descendent directories.</returns>
+        /// <exception cref="NullReferenceException">Thrown if <paramref name="a_this"/> is null.</exception>
+        public static IEnumerable<IDirectory> EnumerateDescendentDirectories(this IDirectory a_this)
+        {
+            #region Argument Validation
+
+            if (a_this == null)
+                throw new NullReferenceException(nameof(a_this));
+
+            #endregion
+
+            foreach (var directory in a_this.Directories)
+            {
+                yield return directory;
+
+                foreach (var descendent in EnumerateDescendentDirectories(directory))
+                    yield return descendent;
+            }
+        }
+
+        /// <summary>
+        /// Enumerate through all descendent files under "this" directory (<paramref name="a_this"/>).
+        /// </summary>
+        /// <param name="a_this">"This" directory.</param>
+        /// <returns>All descendent files.</returns>
+        /// <exception cref="NullReferenceException">Thrown if <paramref name="a_this"/> is null.</exception>
+        public static IEnumerable<IFile> EnumerateDescendentFiles(this IDirectory a_this)
+        {
+            #region Argument Validation
+
+            if (a_this == null)
+                throw new NullReferenceException(nameof(a_this));
+
+            #endregion
+
+            foreach (var file in a_this.Files)
+                yield return file;
+
+            foreach (var directory in a_this.Directories)
+            {
+                foreach (var file in EnumerateDescendentFiles(directory))
+                    yield return file;
+            }
+        }
+
+        /// <summary>
+        /// Get all descendent directories under "this" directory (<paramref name="a_this"/>).
+        /// </summary>
+        /// <param name="a_this">"This" directory.</param>
+        /// <returns>All descendent directories.</returns>
+        public static IDirectory[] GetDescendentDirectories(this IDirectory a_this)
+        {
+            return EnumerateDescendentDirectories(a_this).ToArray();
+        }
+
+        /// <summary>
+        /// Get all descendent files under "this" directory (<paramref name="a_this"/>).
+        /// </summary>
+        /// <param name="a_this">"This" directory.</param>
+        /// <returns>All descendent files.</returns>
+        public static IFile[] GetDescendentFiles(this IDirectory a_this)
+        {
+            return EnumerateDescendentFiles(a_this).ToArray();
+        }
+
         /// <summary>
         /// Copy "this" directory (<paramref name="a_this"/>) to the given destination directory (<paramref name="a_destination"/>).
         /// </summary>
